7b0ad43dcea4513f88e224efe15956edef5ed907
wlgu <782178550@qq.com>
2020-07-20 23:10:37 +0800

    1111

---
+++ apps/my_blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=30, verbose_name='标题')
+    content = models.TextField(verbose_name='内容')
+    pass
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        db_table = 'article'
