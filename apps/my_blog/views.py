c7cef182c0c76c02a340174b7317063c66f73036
v_vwlgu <v_vwlgu@tencent.com>
2020-07-23 18:30:25 +0800

    博客列表展示css 完成

--- apps/my_blog/views.py
+++ apps/my_blog/views.py
@@ -9,15 +9,26 @@ def home(request):
 
 def blog_list(request):
     blogs = Blog.objects.all()
-    return render_to_response('blog/blog_list.html', context={'blogs': blogs})
+    blogs_type = BlogType.objects.all()
+
+    return render_to_response('blog/blog_list.html', context={'blogs': blogs, 'blogs_type': blogs_type})
 
 
 def blog_detail(request, blog_id):
     blog = get_object_or_404(Blog, id=blog_id)
+
     return render_to_response('blog/blog_detail.html', context={'blog': blog})
 
 
 def blogs_with_type(request, blog_type_id):
     blog_type = get_object_or_404(BlogType, id=blog_type_id)
     blogs = Blog.objects.filter(blog_type=blog_type)
-    return render_to_response('blog/blog_with_type.html', context={'blogs': blogs})
+    blogs_type = BlogType.objects.all()
+
+    return render_to_response(
+        'blog/blog_with_type.html',
+        context={
+            'blogs': blogs,
+            'blogs_type': blogs_type,
+            'blog_type': blog_type},
+    )
